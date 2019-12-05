fb3abb4485cca23f7474f82a222e4188096fde69
NullDev (Chris) <shadow@nulldev.org>
2019-12-05 12:07:26 +0100

    refactored data handler

--- src/handler/fileProcessor.js
+++ src/handler/fileProcessor.js
@@ -1,56 +1,57 @@
 "use strict";
 
-let fs = require("fs");
+// ====================== //
+// = Copyright (c) EMMA = //
+// ====================== //
 
+// Core Modules
+let fs = require("fs");
+let path = require("path");
+
+// Utils
 let log = require("../utils/logger");
 let config = require("../utils/configHandler").getConfig();
 
-let getEmotionConfigData = function(emotion){
-    switch(emotion){
-        case "+":  return {
-            directory: config.dataDirectory + config.positiveDirectory,
-            dbTable: config.positiveTable
-        };
-        case "-":  return {
-            directory: config.dataDirectory + config.negativeDirectory,
-            dbTable: config.negativeTable
-        };
-        default : return null;
+// Categorization of constant config entries
+const emotions = [
+    {
+        directory: path.join(config.dataDirectory, config.positiveDirectory),
+        dbTable: config.positiveTable
+    },
+    {
+        directory: path.join(config.dataDirectory, config.negativeDirectory),
+        dbTable: config.negativeTable
     }
-};
+];
 
-let processEmotionData = function(rethink, connection, emotion){
-    const configData = getEmotionConfigData(emotion);
-    fs.readdir(config.dataDirectory, (err, files) => {
-        if (err) return callback(`Unable to scan directory: ${err}`);
+/**
+ * Converts the emotion data
+ *
+ * @param {*} rethink
+ * @param {*} connection
+ * @param {function} callback
+ */
+let processEmotionData = function(rethink, connection, callback){
+    emotions.forEach(emotion =>{
+        // eslint-disable-next-line consistent-return
+        fs.readdir(config.dataDirectory, (err, files) => {
+            if (err) return callback(`Unable to scan directory: ${err}`);
 
-        rethink.table(configData.dbTable)
-            .insert(files.map((file) => {
-                let data = JSON.parse(fs.readFileSync(configData.directory + file, "utf8"));
-                return {
-                    text: data.text.toLowerCase(),
-                    date: data.datetime
-                };
-            }))
-            .run(connection)
-            .then(() => log.info("Insetrted Files!"));
-
+            rethink.table(emotion.dbTable)
+                .insert(files.map((file) => {
+                    let data = JSON.parse(fs.readFileSync(path.join(emotion.directory, file), "utf8"));
+                    return {
+                        text: data.text.toLowerCase(),
+                        date: data.datetime
+                    };
+                }))
+                .run(connection)
+                .then(() => log.info("Insetrted Files!"));
+        });
         return callback(null);
     });
 };
 
-
-/**
- * Process the actual data.
- *
- * @param {*} rethink
- * @param {*} connection
- */
-let processRawTweetData = function(rethink, connection, callback){
-    processEmotionData(rethink, connection, "+");
-    processEmotionData(rethink, connection, "-");
-};
-
 module.exports = {
-    processRawTweetData
+    processEmotionData
 };
