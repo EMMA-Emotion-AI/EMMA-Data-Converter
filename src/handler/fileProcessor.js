5018e8b5a2c7a3dd166dd716e5891cd5b1ebb4b5
Unknown <joseph.juri@gmail.com>
2019-12-05 11:39:40 +0100

    Emotion wip

--- src/handler/fileProcessor.js
+++ src/handler/fileProcessor.js
@@ -5,19 +5,28 @@ let fs = require("fs");
 let log = require("../utils/logger");
 let config = require("../utils/configHandler").getConfig();
 
-/**
- * Process the actual data.
- *
- * @param {*} rethink
- * @param {*} connection
- */
-let processRawTweetData = function(rethink, connection, callback){
+let getEmotionConfigData = function(emotion){
+    switch(emotion){
+        case "+":  return {
+            directory: config.dataDirectory + config.positiveDirectory,
+            dbTable: config.positiveTable
+        };
+        case "-":  return {
+            directory: config.dataDirectory + config.negativeDirectory,
+            dbTable: config.negativeTable
+        };
+        default : return null;
+    }
+};
+
+let processEmotionData = function(rethink, connection, emotion){
+    const configData = getEmotionConfigData(emotion);
     fs.readdir(config.dataDirectory, (err, files) => {
         if (err) return callback(`Unable to scan directory: ${err}`);
 
-        rethink.table("positive")
+        rethink.table(configData.dbTable)
             .insert(files.map((file) => {
-                let data = JSON.parse(fs.readFileSync(config.dataDirectory + file, "utf8"));
+                let data = JSON.parse(fs.readFileSync(configData.directory + file, "utf8"));
                 return {
                     text: data.text.toLowerCase(),
                     date: data.datetime
@@ -28,6 +37,18 @@ let processRawTweetData = function(rethink, connection, callback){
 
         return callback(null);
     });
+};
+
+
+/**
+ * Process the actual data.
+ *
+ * @param {*} rethink
+ * @param {*} connection
+ */
+let processRawTweetData = function(rethink, connection, callback){
+    processEmotionData(rethink, connection, "+");
+    processEmotionData(rethink, connection, "-");
 };
 
 module.exports = {
